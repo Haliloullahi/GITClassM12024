4ddde1f16ffca67b9713c0070ebef32c517e4688
Halil <ibrahima.toure.etu@esmt.sn>
2024-03-04 04:51:13 +0000

    Mon premier commit

---
+++ GITClassM12024/VSCClass.java
@@ -0,0 +1,10 @@
+public class ListeJoursSemaine {
+    public static void main(String[] args) {
+        String[] joursSemaine = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
+
+        System.out.println("Les jours de la semaine sont :");
+        for (String jour : joursSemaine) {
+            System.out.println(jour);
+        }
+    }
+}
